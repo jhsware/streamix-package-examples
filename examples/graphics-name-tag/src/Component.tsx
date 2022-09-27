ead0e904655887a1139062799aac36a1ba26f820
Sebastian Ware <sebastian@urbantalk.se>
2022-09-27 20:03:23 +0200

    Another example and added docs for examples

---
+++ examples/graphics-name-tag/src/Component.tsx
@@ -0,0 +1,29 @@
+import { componentDidAppear, componentWillDisappear } from 'inferno-animation';
+
+type TInfernoFnCompProps = {
+  value: object;
+  onComponentDidAppear?: (dom: HTMLElement, props) => void;
+  onComponentWillDisappear?: (dom: HTMLElement, props: any, callback: Function) => void;
+  animation: string;
+}
+
+function NameTag({value, animation}: TInfernoFnCompProps) {
+  return (
+    <div className="NameTag">
+      <div className="inner">
+        <span className="name">{value['name']}</span>
+        <span className="title">{value['title']}</span>
+      </div>
+    </div>
+  )
+}
+
+export default function Container({id, name, isStaged, data}) {
+  return <div className="name-tag">
+    {isStaged && <NameTag
+      value={data}
+      onComponentDidAppear={componentDidAppear}
+      onComponentWillDisappear={componentWillDisappear}
+      animation="NameTag" />}
+  </div>
+}
--- examples/video-player/src/Component.tsx
+++ examples/video-player/src/Component.tsx
@@ -1,7 +1,12 @@
 import { name } from './streamix_package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default function Container({id, isNext, isStaged, data}) {
+  const videoSrc = isProd
+    ? `videos/${data.videoFileName}`
+    : `packages/${name}/videos/${data.videoFileName}`;
   return <div className="video-player">
-    {(isNext || isStaged) && <video autoPlay={isStaged} src={`videos/${data.videoFileName}`} preload="metadata"></video>}
+    {(isNext || isStaged) && <video autoPlay={isStaged} src={videoSrc} preload="metadata"></video>}
   </div>
 }
